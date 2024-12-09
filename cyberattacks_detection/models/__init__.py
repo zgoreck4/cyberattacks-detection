a7d420d80375a530bbdc077b9be89a2baff0c3e9
zgoreck4 <zgorecka@o2.pl>
2024-12-09 13:42:08 +0100

    add min_max_scale and reverse_min_max_scale func to utils

--- cyberattacks_detection/models/__init__.py
+++ cyberattacks_detection/models/__init__.py
@@ -1,9 +1,12 @@
 from .ELM import ELM
 from .RBFNN import RBFNN
 from .ModelWrapper import ModelWrapper
+from .utils import min_max_scale, reverse_min_max_scale
 
 __all__ = [
     "ELM",
     "RBFNN",
-    "ModelWrapper"
+    "ModelWrapper",
+    "min_max_scale",
+    "reverse_min_max_scale"
 ]
--- cyberattacks_detection/models/utils.py
+++ cyberattacks_detection/models/utils.py
@@ -7,3 +7,9 @@ def gaussian_func(x, c, sigma):
 def grad_gaussian_func(x, c, sigma):
     distance = np.linalg.norm(x - c, 2)
     return gaussian_func(x, c, sigma) * (distance**2) * 1/(sigma**3)
+
+def min_max_scale(s, min_value, max_value):
+    return (s-min_value)/(max_value - min_value)
+
+def reverse_min_max_scale(s, min_value, max_value):
+    return s * (max_value - min_value) + min_value
