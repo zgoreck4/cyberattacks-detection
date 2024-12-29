e5f75e9fd86a57dee440aba6c25333b4f48c2fd7
zgoreck4 <zgorecka@o2.pl>
2024-12-29 15:10:20 +0100

    add example_use_simulation_all_models.py

---
+++ cyberattacks_detection/scripts/example_use_simulation_all_models.py
@@ -0,0 +1,351 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cycler
+from pathlib import Path
+from ..simulation import Simulation
+import pickle
+from ..models import ELM, RBFNN
+from ..detection import CyberattackDetector
+from matplotlib.ticker import MaxNLocator
+import re
+from sklearn import metrics
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+def main_function() -> None:
+
+    save_mode = True
+    close_loop = True
+    attack_scenario = 2 # 3
+    num_tank = 0
+    attack_value = 0.05
+    tau_y_ca = 50
+    # model_type = 'lr' # None
+    recursion_mode = True
+    window_detection = 100
+    threshold_method = 'z-score' # 'percentile'
+
+    tau_u = 0
+    tau_y = 0
+    # h0 = [65, 66, 65, 66]
+    # dla gamma_a = 0.3, gamma_b = 0.4
+    h0 = [65.37515073890378, 64.98201463176996, 65.90206440432354, 65.8157923349714]
+    # h0 = [60.88756594079625, 68.99253334710369, 0, 0]
+    # dla gamma = 0.8
+    # h0 = [66.54149974283987, 63.981536657570146, 7.322451600480413, 5.372717741630307]
+    # dla gamma_a = 0.7, gamma_b = 0.6
+    # h0 = [86.33849517038516, 49.20411218062061, 29.28980640192163, 12.088614918668185]
+    # h0 = [12.4, 1.8, 12.7, 1.4]
+    # h0 = [12.6, 4.8, 13, 4.9]
+    active_noise = False # wartość False wyłącza zakłócenia, wartość True włącza
+    noise_sigma = 0.1
+    e_sigma = 0.005
+
+    h_max = [[136],
+            [136],
+            [130],
+            [130]]
+    h_min = [[20],
+            [20],
+            [20],
+            [20]]
+
+    qa_max = 3260000/3600
+    qb_max = 4000000/3600
+    q_min = 0
+    gamma_a = 0.3
+    gamma_b = 0.4
+    S = np.array([60, 60, 60, 60])
+    a = np.array([1.31, 1.51, 0.927, 0.882]) # przekrój otworu wylotowego
+    c = np.array([1, 1, 1, 1])
+
+    kp = 2
+    Ti = 15 # 1000000000000000000000
+    Td = 0 # 1.5
+
+    # cm
+    step_dur = 3000/5 # 200
+
+    SP_h = None
+    q = None
+
+    model_path = Path(__file__).parent.parent / "saved_models"
+
+    h = []
+    y = []
+    z = []
+    q = []
+    e = []
+    h_model = []
+    attack_signal = []
+
+    model_type_tuple = ('lr', 'elm', 'rbf')
+    model_dict = {'lr':'Regresja liniowa', 'elm': 'Sieć ELM', 'rbf': 'Sieć RBF'}
+
+    for model_type in model_type_tuple:
+
+        if model_type == 'lr':
+            model1 = pickle.load(open(f"{model_path}/lr_x1.sav", 'rb'))
+            model2 = pickle.load(open(f"{model_path}/lr_x2.sav", 'rb'))
+            model3 = pickle.load(open(f"{model_path}/lr_x3.sav", 'rb'))
+            model4 = pickle.load(open(f"{model_path}/lr_x4.sav", 'rb'))
+            model_list = [model1, model2, model3, model4]
+        elif model_type == 'elm':
+            model1 = ELM(0, 0)
+            model1.load_model(f"{model_path}/elm_x1.npz")
+            model2 = ELM(0, 0)
+            model2.load_model(f"{model_path}/elm_x2.npz")
+            model3 = ELM(0, 0)
+            model3.load_model(f"{model_path}/elm_x3.npz")
+            model4 = ELM(0, 0)
+            model4.load_model(f"{model_path}/elm_x4.npz")
+            model_list = [model1, model2, model3, model4]
+        elif model_type == 'rbf':
+            model1 = RBFNN(None)
+            # model1.load_model(f"{model_path}/rbf_x1.npz")
+            model1.load_model(f"{model_path}/rbf_x1_v0.npz")
+            model2 = RBFNN(None)
+            model2.load_model(f"{model_path}/rbf_x2.npz")
+            model3 = RBFNN(None)
+            model3.load_model(f"{model_path}/rbf_x3.npz")
+            model4 = RBFNN(None)
+            model4.load_model(f"{model_path}/rbf_x4.npz")
+            model_list = [model1, model2, model3, model4]
+        else:
+            model_list = None
+
+        # należy ustawić próg w detektorze na podstawie normalnej pracy
+        if (attack_scenario is not None) and model_list is not None:
+            SP_h1 = np.array([h0[0], h0[0], 80, 80, 100, 100, 90])
+            SP_h2 = np.array([h0[1], 55,   55, 95, 95, 105, 105])
+            SP_h = np.vstack((SP_h1, SP_h2))
+            SP_h = np.repeat(SP_h, step_dur, axis=1)
+
+            n_sampl = np.shape(SP_h)[1]
+            T_s = 1
+            T = n_sampl // T_s # TODO: sprawdzić działanie jeżeli n_sampl nie dzieli się całkowicie przez T_s
+            time = np.arange(0, T, T_s)
+            T = max(time)
+            qd = np.round(np.random.randn(4,n_sampl)*noise_sigma*active_noise, 4)
+            simulation_normal = Simulation(h_max, h_min, qa_max, qb_max, gamma_a, gamma_b,
+                                    S, a, c, T, T_s, kp, Ti, Td, tau_u, tau_y, qd
+                                    # , noise_sigma, e_sigma
+                                    )
+            h_normal, _, _, _, _, h_model_normal, _ = simulation_normal.run(h0,
+                                        close_loop,
+                                        model_list=model_list,
+                                        recursion_mode=recursion_mode,
+                                        SP_h=SP_h,
+                                        q=q,
+                                        qa0=1630000/3600,
+                                        qb0=2000000/3600,
+                                        attack_scenario=None)
+            
+            cyberattack_detector = CyberattackDetector(window=window_detection)
+            cyberattack_detector.calc_threshold(h_normal[:len(h_model_normal), :], h_model_normal, method=threshold_method)
+
+            # plt.figure(figsize=(8, 9))
+            # plt.title("Poziom rzeczywisty i przewidywany w stanie normalnym")
+            # for i, (h_i, h_model_i) in enumerate(zip(h_normal, h_model_normal)):
+            #     ax1 = plt.subplot(4, 1, i+1)
+            #     ax1.plot(time, h_i, label=f'pomiar $h_{i+1}$')
+            #     # plt.axhline(y=h_max[i], color='black', linestyle='--', label=f'h_max{i+1}')
+            #     # plt.axhline(y=h_min[i], color='black', linestyle='--', label=f'h_min{i+1}')
+            #     if model_type is not None:
+            #         ax1.plot(time, h_model_i, linestyle='--', label=rf'model {model_type.upper()} $\hat{{h_{i+1}}}$')
+            #     ax1.set_xlabel('t [s]')
+            #     ax1.set_ylabel(f'$h_{i+1} [cm]$')
+            #     # ax1.title(f"Poziom wody w {i+1} zbiorniku")
+            #     ax1.legend(loc='best', bbox_to_anchor=(0, 0, 0.5, 1.0))
+            #     ax1.grid()
+            # plt.show()
+
+        else:
+            cyberattack_detector = None
+
+        SP_h1 = np.array([h0[0], h0[0], 70, 70, 95, 95, 90])
+        SP_h2 = np.array([h0[1], 50,   50, 90, 90, 105, 105])
+        # SP_h1 = np.array([h0[0], 80])
+        # SP_h2 = np.array([h0[1], h0[1]])
+        SP_h = np.vstack((SP_h1, SP_h2))
+        SP_h = np.repeat(SP_h, step_dur, axis=1)
+        # print(f"{SP_h}")
+        n_sampl = np.shape(SP_h)[1]
+
+        T_s = 1
+        T = n_sampl // T_s # TODO: sprawdzić działanie jeżeli n_sampl nie dzieli się całkowicie przez T_s
+        time = np.arange(0, T, T_s)
+        T = max(time)
+
+        attack_time = n_sampl//2
+
+        qd = np.round(np.random.randn(4,n_sampl)*noise_sigma*active_noise, 4)
+
+        simulation = Simulation(h_max, h_min, qa_max, qb_max, gamma_a, gamma_b,
+                                S, a, c, T, T_s, kp, Ti, Td, tau_u, tau_y, qd, cyberattack_detector=cyberattack_detector
+                                # , noise_sigma, e_sigma
+                                )
+
+        hi, yi, zi, qi, ei, h_modeli, attack_signali = simulation.run(h0,
+                                    close_loop,
+                                    model_list=model_list,
+                                    recursion_mode=recursion_mode,
+                                    SP_h=SP_h,
+                                    q=q,
+                                    qa0=1630000/3600,
+                                    qb0=2000000/3600,
+                                    attack_scenario=attack_scenario,
+                                    num_tank=num_tank,
+                                    attack_time=attack_time,
+                                    attack_value=attack_value,
+                                    tau_y_ca=tau_y_ca)
+        h.append(hi)
+        y.append(yi)
+        z.append(zi)
+        q.append(qi)
+        e.append(ei)
+        y.append(yi)
+        h_model.append(h_modeli)
+        attack_signal.append(attack_signali)
+
+    # Set global font sizes for different elements
+    plt.rcParams.update({
+        'axes.titlesize': 20,    # Titles of subplots
+        'axes.labelsize': 14,     # Labels for axes
+        'axes.prop_cycle': cycler.cycler(
+            color=['tab:blue',
+                   'tab:orange',
+                   'tab:green',
+                   'tab:brown',
+                   'tab:purple',
+                   'tab:cyan',
+                   'tab:pink',
+                   'tab:olive']
+            ),
+        'xtick.labelsize': 14,    # X-axis tick labels
+        'ytick.labelsize': 14,    # Y-axis tick labels
+        'legend.fontsize': 14,     # Legend font size
+        'figure.titlesize': 24    # Overall figure title size (if used)
+    })
+
+    plot_path = Path(__file__).parent.parent / "plots/SDM2"
+    
+    result_path = Path(__file__).parent.parent / "results"
+
+    attack_binary = np.hstack((np.zeros(attack_time+1), np.ones(T-attack_time)))
+    
+    def calc_NRMSE(true, predict, y_name_idx):
+        RMSE = metrics.root_mean_squared_error(true, predict)    
+        return RMSE/(h_max[0][y_name_idx] - h_min[0][y_name_idx])
+
+    if attack_scenario is None:
+
+        fig2 = plt.figure(figsize=(13, 9))
+        # fig2.suptitle("Stan Normalny procesu")
+
+        ax1 = plt.subplot(3, 1, 1, sharex=None)
+        ax1.plot(time, q[0][0], label='$q_A$')
+        ax1.plot(time, q[0][1], label='$q_B$')
+        # plt.axhline(y=qa_max, color='black', linestyle='--', label='Pompa A max')
+        # plt.axhline(y=qb_max, color='grey', linestyle='--', label='Pompa B max')
+        ax1.set_ylabel('$q [cm^3/s]$')
+        ax1.set_title('Przepływ pomp')
+        ax1.legend(loc='center left')
+        ax1.grid()
+
+        ax2 = plt.subplot(3, 1, 2, sharex=ax1)
+        ax2.plot(time, z[0][0], label='$h_1$')
+        # TODO: do wyrzucenia???
+        ax2.plot(time, z[0][1], label='$h_2$')
+        print(np.hstack((time, [max(time)+1])))
+        ax2.step(time, SP_h[0], label='$w_1$', linestyle='-.')
+        ax2.step(time, SP_h[1], label='$w_2$', linestyle=(0, (5, 5)))
+        ax2.set_ylabel('h [cm]')
+        ax2.set_title("Zmierzony i zadany poziom w zbiornikach")
+        ax2.legend(loc='center left')
+        ax2.grid()
+
+        ax3 = plt.subplot(3, 1, 3, sharex=ax1)
+        ax3.plot(time, z[0][num_tank], label='$h_1$')
+        # h_model_i - NDArray z wynikami dla modeli 1 typu, ale różnych zbiorników
+        for h_model_i, model_type in zip(h_model, model_type_tuple):
+            ax3.plot(time, h_model_i[num_tank], linestyle='--', label=rf'model {model_type.upper()} $\hat{{h_1}}$')
+        ax3.set_xlabel('t [s]')
+        ax3.set_ylabel('h [cm]')
+        ax3.set_title("Rzeczywisty i modelowany poziom w 1. zbiorniku")
+        ax3.legend(loc='center left')
+        ax3.grid()
+
+        plt.subplots_adjust(hspace=0.5)
+
+        if save_mode:
+            plt.savefig(f"{plot_path}/SP_PV_rec_{recursion_mode}_att{attack_scenario}_tank{num_tank}_value{attack_value}_tau_y{tau_y_ca}_window{window_detection}_method_{threshold_method}.png",
+                        bbox_inches ='tight')
+        plt.show()
+
+        for h_i, h_model_i, model_type in zip(h, h_model, model_type_tuple):
+            NRMSE = calc_NRMSE(h_i[num_tank], h_model_i[num_tank], 0)
+            print(f"NRMSE {model_type.upper()}: {NRMSE:.4f}")
+
+    else:
+
+        result_df = pd.DataFrame(columns=['Opóźnienie [s]', 'Recall', 'FPR'])
+
+        for model_type, attack_signal_i in zip(model_type_tuple, attack_signal):
+            attack_res = pd.DataFrame()
+            attack_res['true'] = attack_binary
+            attack_res['pred'] = attack_signal_i[:, num_tank]
+            attack_res.dropna(inplace=True)
+            attack_res = attack_res.astype(int)
+
+            recall = metrics.recall_score(attack_res['true'], attack_res['pred'])
+            tn, fp, fn, tp = confusion_matrix(attack_res['true'], attack_res['pred']).ravel()
+            fpr = fp / (fp + tn)
+
+            indices = np.where((attack_binary[:-1] == 0) & (attack_binary[1:] == 1))[0]
+            attack_time = indices[0]
+            indices = np.where((attack_signal_i[:-1] == 0) & (attack_signal_i[1:] == 1))[0]
+            indices = indices[indices>attack_time]
+            if len(indices) == 0:
+                attack_time_delay = None
+            else:
+                detected_attack_time = indices[0]
+                attack_time_delay = detected_attack_time - attack_time
+            print(attack_time_delay)
+
+            result_df.loc[model_type.upper()] = [attack_time_delay, round(recall, 4), round(fpr, 4)]
+            result_df.index.name = 'Model'
+
+        result_df.to_excel(f"{result_path}/result_df_{recursion_mode}_att{attack_scenario}_tank{num_tank}_value{attack_value}_tau_y{tau_y_ca}_window{window_detection}_method_{threshold_method}.xlsx")
+        
+        fig2=plt.figure(figsize=(13, 9))
+        # fig2.suptitle(f'Poziom wody w 2 zbiornikach {title_part} w trybie {title_recursion}')
+        fig2.suptitle(f'Poziom cieczy w 1. zbiorniku')
+        for i, (zi, hi, h_modeli, model_type, attack_signali) in enumerate(zip(z, h, h_model, model_type_tuple, attack_signal)):
+            ax1 = plt.subplot(3, 1, i+1)
+            ax1.plot(time, zi[num_tank], label=f'pomiar $h_{num_tank+1}$')
+            ax1.plot(time, hi[num_tank], linestyle='-.', label=f'rzecz. $h_{num_tank+1}$')
+            ax1.plot(time, h_modeli[num_tank], linestyle='--', label=rf'$\hat{{h_{num_tank+1}}}$')
+            ax1.set_xlabel('t [s]')
+            ax1.set_ylabel(f'$h_{num_tank+1} [cm]$')
+            ax1.set_title(f"{model_dict[model_type]}")
+            ax1.grid()
+            # Add secondary y-axis to the first subplot
+            ax1_secondary = ax1.twinx()
+            ax1_secondary.plot(time, attack_binary, color='red', linestyle='--', label='cyberatak')
+            ax1_secondary.fill_between(time, attack_signali[:, num_tank].astype(float), color='r', alpha=0.2, label=f'wykryty cyberatak')
+            ax1_secondary.set_ylabel('Sygnał binarny\ncyberataku')
+            # set y-axis to only show integer values
+            ax1_secondary.yaxis.set_major_locator(MaxNLocator(integer=True))
+            ax1_secondary.legend(loc='best', bbox_to_anchor=(0.8, 0.1, 0.2, 0.8))
+            ax1.legend(loc='best', bbox_to_anchor=(0, 0, 0.2, 1.0))
+
+        plt.subplots_adjust(hspace=0.6)
+
+        if save_mode:
+            plt.savefig(f"{plot_path}/h_rec_{recursion_mode}_att{attack_scenario}_tank{num_tank}_value{attack_value}_tau_y{tau_y_ca}_window{window_detection}_method_{threshold_method}.png",
+                        bbox_inches ='tight')
+        plt.show()
+
+if __name__ == "__main__":
+    main_function()
